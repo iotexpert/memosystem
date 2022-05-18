a6dae0003e12ba364a5027722313315508075679
ggearing <ggearing@enovix.com>
2022-05-18 12:01:10 -0500

    more testing of account page. Coverage 95 percent.

--- memos/tests/functional/test_account.py
+++ memos/tests/functional/test_account.py
@@ -1,7 +1,5 @@
 def test_register(client, session):
-    """
-    Test base / url
-    """
+    # Register new user
     response = client.post('/register', 
         data=dict(username='jdoe',email='john.doe@gmail.com',password='pass', confirm_password='pass', submit='Sign Up'),
         follow_redirects=True)
@@ -17,9 +15,76 @@ def test_register(client, session):
     assert b'That username is taken. Please choose a different one' in response.data
     assert b'That email is taken. Please choose a different one.' in response.data
 
+    # attempt login with bad password
+    response = client.post('/login', 
+        data=dict(email='john.doe@gmail.com',password='bad pass', submit='Login'),
+        follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Login Unsuccessful' in response.data
+
+    # successful login
     response = client.post('/login', 
         data=dict(email='john.doe@gmail.com',password='pass', submit='Login'),
         follow_redirects=True)
     assert response.status_code == 200
     assert b'Account: jdoe' in response.data
     assert b'Logout' in response.data
+
+    # When logged in, /register redirects to home page
+    response = client.get('/register',
+        follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Account: jdoe' in response.data
+    assert b'Logout' in response.data
+
+    # When logged in, /login redirects to home page
+    response = client.get('/login',
+        follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Account: jdoe' in response.data
+    assert b'Logout' in response.data
+
+def test_account_access_admin(client, session):
+    # login as admin
+    response = client.post('/login',
+                            data=dict(email='adminUser@gmail.com', password='u'),
+                            follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Account: adminUser' in response.data
+
+    # View page for self
+    response = client.get('/account', follow_redirects=True)
+    assert response.status_code == 200
+    assert b'adminUser@gmail.com' in response.data
+    assert b'Account Info' in response.data
+
+    # View page for invalid account
+    response = client.get('/account/badUser', follow_redirects=True)
+    assert response.status_code == 404
+    assert b'Account Info' not in response.data
+
+    # View page for other account
+    response = client.get('/account/avgUser', follow_redirects=True)
+    assert response.status_code == 200
+    assert b'avgUser@gmail.com' in response.data
+    assert b'Account Info' in response.data
+
+def test_account_access_nonadmin(client, session):
+    # login as normal user
+    response = client.post('/login',
+                            data=dict(email='avgUser@gmail.com', password='u'),
+                            follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Account: avgUser' in response.data
+
+    # View page for self
+    response = client.get('/account', follow_redirects=True)
+    assert response.status_code == 200
+    assert b'avgUser@gmail.com' in response.data
+    assert b'Account Info' in response.data
+
+    # View page for other account
+    response = client.get('/account/adminUser', follow_redirects=True)
+    assert response.status_code == 200
+    assert b'adminUser@gmail.com' in response.data
+    assert b'Account Info' in response.data
