74ed7fa33df7e23c54ea6d57b473083b470d7b8d
ggearing <ggearing@enovix.com>
2022-05-24 19:31:27 -0500

    #51 Fix email hang by setting From to same user as authenticated to mail server.

--- memos/memos/users/utils.py
+++ memos/memos/users/utils.py
@@ -26,13 +26,18 @@ def save_picture(form_picture):
 
 
 def send_reset_email(user):
-    token = user.get_reset_token()
-    msg = Message('Password Reset Request',
-                  sender='noreply@demo.com',
-                  recipients=[user.email])
-    msg.body = f'''To reset your password, visit the following link:
-{url_for('users.reset_token', token=token, _external=True)}
+    try:
+        token = user.get_reset_token()
+        msg = Message('Password Reset Request',
+                    sender=os.environ['MEMOS_EMAIL_USER'],
+                    recipients=[user.email])
+        msg.body = f'''To reset your password, visit the following link:
+    {url_for('users.reset_token', token=token, _external=True)}
 
-If you did not make this request then simply ignore this email and no changes will be made.
-'''
-    mail.send(msg)
+    If you did not make this request then simply ignore this email and no changes will be made.
+    '''
+        mail.send(msg)
+
+    except BaseException as e:
+        raise e
+    return True    
