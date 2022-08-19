9011701d203145e8cc0cd206e0537f52ae1e6531
iotexpert <engineer@iotexpert.com>
2022-08-19 10:11:07 -0400

    fixed @app decorator

--- memos/app.py
+++ memos/app.py
@@ -1,5 +1,7 @@
 from os.path import exists,join
 from shutil import copy
+
+from memos.create import create_app
 from memos.models.Memo import Memo
 
 temp_sl = join("memos","static","config","settings_local.py")
@@ -12,16 +14,7 @@ try:
 except ImportError:
     pass
 
-
-from memos import create_app
-
-
 app = create_app()
-
-# This will make the static member function "get_pinned" available in the template
-@app.context_processor
-def inject_pinned():
-    return dict(get_pinned=Memo.get_pinned)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- memos/memos/__init__.py
+++ memos/memos/__init__.py
@@ -6,8 +6,10 @@ from flask_mail import Mail
 from flaskext.markdown import Markdown
 from flask_migrate import Migrate
 
+
 from memos.extensions import ldap
 from memos.config import Config
+
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -15,35 +17,4 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
-
 migrate = None
-
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    Markdown(app)
-    app.config.from_object(Config)
-
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    mail.init_app(app)
-    migrate = Migrate(app, db)
-    
-    if ldap:
-        ldap.init_app(app) #pragma nocover  -- testing ldap is very environment centric.
-
-    from memos.users.routes import users
-    from memos.main.routes import main
-    from memos.memos.routes import memos
-    from memos.errors.handlers import errors
-
-    from memos.admin.routes import admin
-
-    app.register_blueprint(users)
-    app.register_blueprint(memos)
-    app.register_blueprint(main)
-    app.register_blueprint(errors)
-    app.register_blueprint(admin)
-
-    return app
-
